9b3f6ae11114d65820761b90fb2df146db60c3ee
oliviasv <62649380+oliviasv@users.noreply.github.com>
2021-01-22 16:45:34 +0200

    cypress configuration

--- cypress/integration/examples/sample.spec.js
+++
@@ -1,9 +0,0 @@
-describe('My First Real Test', function() {
-    it('Visits the Counter App', function() {
-        cy.visit('http://localhost:3001/')
-
-        cy.pause()
-
-        cy.contains('Items')
-    })
-})
