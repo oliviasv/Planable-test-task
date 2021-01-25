b2273290ce6b758aaa3a980de993079bfe449164
oliviasv <62649380+oliviasv@users.noreply.github.com>
2021-01-25 14:38:12 +0200

    test each individual counter

--- cypress/integration/main_page.spec.js
+++ cypress/integration/main_page.spec.js
@@ -4,43 +4,34 @@ describe('Test Counter App main page', function() {
     })
 
     it('Verify if there are 0 items when the app is loaded', function() {
-        cy.get('.badge-info').should('contain', '0')
-        // cy.get('.btn-secondary').first().click()
-        // cy.get('.badge-info').should('contain', '1')
+        cy.get('.badge-info').should('contain', '0')  
     })
 
-    it('Test if each individual counter have the Zero value', function() {
-        cy.get('div > .row').each( function() {
-            cy.get('.badge-warning').should('contain', 'Zero');
+    it('Test each individual counter', function() {
+        cy.get('div > .row').each(() => {
+            cy.get('.badge-warning').should('contain', 'Zero')    
+        })
+
+        cy.get('.btn-secondary').click({multiple: true})
+        //   .each(($div, index, $divs) => {})
+          .then(($divs) => {
+            expect($divs).to.have.length(4)         
+        }) 
+        
+        cy.get('div > .row').each(() => {
+            cy.get('.badge-primary').should('contain', '1')
+        })
+
+
+        cy.get('.btn-info').click({multiple: true})
+        //   .each(($div, index, $divs) => {})
+          .then(($divs) => {
+            expect($divs).to.have.length(4)
+        }) 
+      
+        cy.get('div > .row').each(() => {
+            cy.get('.badge-warning').should('contain', 'Zero')    
         })
     })
 
-    it('The value increases by 1 when plus button is clicked', function() {
-        cy.get('.col-md-1 > span').then(($span) => {
-            const num1 = parseFloat($span.text())
-
-            cy.get('.btn-secondary').first().click().then( () => {
-                const num2 = parseFloat($span.text())
-
-            expect(num2).to.eq(num1 + 1)
-            })
-        })
-    })
-
-    it('The value deccreases by 1 when minus button is clicked', function() {
-        cy.get('.col-md-1 > span').then(($span) => {
-            const num1 = parseFloat($span.text())
-
-            cy.get('.btn-info').first().click().then( () => {
-                const num2 = parseFloat($span.text())
-
-            expect(num2).to.eq(num1 - 1)
-            })
-        })
-    })
-
-    it('Verify the delete button', function() {
-        cy.get('.btn-danger').first().click()
-        cy.get('.row').should('not.be.visible')
-    })
 })
