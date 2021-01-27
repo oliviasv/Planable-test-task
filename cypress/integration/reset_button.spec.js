52393773fd024739a39b048769575fd3e1b86219
oliviasv <62649380+oliviasv@users.noreply.github.com>
2021-01-27 20:38:42 +0200

    update reset button test

--- cypress/integration/reset_button.spec.js
+++ cypress/integration/reset_button.spec.js
@@ -1,22 +1,18 @@
-describe('Reset Button', function()  {
+describe('Reset Button should refresh all items', function()  {
     beforeEach(() => {
         cy.visit('/')
     })
 
-    it('all counters returned to Zero on click', function() {
+    it('reset all items that have been incremented', function() {
+        
         let num1 = Math.floor(Math.random() * 10) 
 
         for(let n = 0; n < num1; n++) {
             cy.get('[data-cy=plus-button]').click({multiple: true})
-        //   .each(($div, index, $divs) => {})
-            .then(($divs) => {
-                expect($divs).to.have.length(4)        
-            })  
         }
 
         cy.get('[data-cy=reset-button]').click()
-        cy.get('[data-cy=individual-counter]').each(() => {
-            cy.get('[data-cy=badge-value]').should('contain', 'Zero')    
-        })
+        
+        cy.get('[data-cy=badge-value]').should('contain', 'Zero')    
     })
 })
